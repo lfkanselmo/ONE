e595e8cd3cc84e67e5d68ce384fdebab9cd463c6
Anselmo Ortega <127802328+lfkanselmo@users.noreply.github.com>
2023-09-21 16:52:04 -0500

    progreso proyecto api-rest

---
+++ Java + Spring/Spring Project/api/src/main/java/med/voll/api/medico/DatosListadoMedico.java
@@ -0,0 +1,13 @@
+package med.voll.api.medico;
+
+public record DatosListadoMedico(
+        Long id,
+        String nombre,
+        String especialidad,
+        String docuemento,
+        String email) {
+
+    public DatosListadoMedico(Medico medico){
+        this(medico.getId(), medico.getNombre(), medico.getEspecialidad().toString(), medico.getDocumento(), medico.getEmail());
+    }
+}
--- Java + Spring/Spring Project/api/src/main/java/med/voll/api/medico/Medico.java
+++ Java + Spring/Spring Project/api/src/main/java/med/voll/api/medico/Medico.java
@@ -17,17 +17,95 @@ public class Medico {
     private Long id;
     private String nombre;
     private String email;
+    private String telefono;
     private String documento;
+    private boolean activo;
     @Enumerated(EnumType.STRING)
     private Especialidad especialidad;
     @Embedded
     private Direccion direccion;
 
     public Medico(DatosRegistroMedico datosRegistroMedico) {
+        this.activo = true;
         this.nombre = datosRegistroMedico.nombre();
         this.email = datosRegistroMedico.email();
+        this.telefono = datosRegistroMedico.telefono();
         this.especialidad = datosRegistroMedico.especialidad();
         this.documento = datosRegistroMedico.documento();
         this.direccion = new Direccion(datosRegistroMedico.direccion());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public void actualizarDatos(DatosActualizarMedico datosActualizarMedico) {
+        if (datosActualizarMedico.nombre() != null){
+            this.nombre = datosActualizarMedico.nombre();
+        }
+
+        if(datosActualizarMedico.documento() != null){
+            this.documento = datosActualizarMedico.documento();
+        }
+
+        if (datosActualizarMedico.direccion() != null){
+            this.direccion = direccion.actualizarDatos(datosActualizarMedico.direccion());
+        }
+    }
+
+    public void desactivarMedico() {
+        this.activo = false;
     }
 }
